229e953613b27c9a83ace90e85cf9f128301630a
Emily Campbell <emilyacamp21@gmail.com>
2025-02-24 18:14:32 -0500

    empty prod branch

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { WelcomeComponent } from './welcome/welcome.component';
-
-export const routes: Routes = [
-    { path: 'welcome', component: WelcomeComponent },
-    { path: '*', component: WelcomeComponent },
-    { path: '', component: WelcomeComponent },
-];
