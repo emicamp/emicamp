8efa297d45225f596ce8a7deb3fc3be060321bcb
Emily Campbell <emilyacamp21@gmail.com>
2025-02-25 17:38:56 -0500

    workflow is good and image path update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
