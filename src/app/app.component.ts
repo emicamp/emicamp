fe040ed3edc26ae1f7b7ff05cc0b9b149b12500d
Emily Campbell <emilyacamp21@gmail.com>
2025-02-25 17:45:28 -0500

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './navigation/navigation.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   standalone: true,
   imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.css'
 })
-export class HeaderComponent {
+export class NavigationComponent {
 
 }
